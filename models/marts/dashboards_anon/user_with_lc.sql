22a0aacbf37a01190c973e14fea5463d9854b27d
Sam Pibworth <sam.pibworth@lumilinks.com>
2022-09-09 11:17:21 +0100

    fix users lc

--- models/marts/dashboards_anon/user_with_lc.sql
+++ models/marts/dashboards_anon/user_with_lc.sql
@@ -3,9 +3,9 @@ WITH mock_brands AS (
     FROM {{ref('trans__mock_brands')}}
 )
 
-,lc_register AS (
+,lc_add AS (
     SELECT *
-    FROM {{ref('trans__lc_registered')}}
+    FROM {{ref('src__fact_lc_add')}}
 )
 
 ,lc_removed AS (
@@ -44,13 +44,13 @@ WITH mock_brands AS (
 
 ,lc_start_end as (
     SELECT
-        lcj.LOYALTY_CARD_ID
-        ,lcj.USER_ID
-        ,DATE(lcj.EVENT_DATE_TIME) AS LC_START_DATE
+        lca.LOYALTY_CARD_ID
+        ,lca.USER_ID
+        ,DATE(lca.EVENT_DATE_TIME) AS LC_START_DATE
         ,DATE(lcr.EVENT_DATE_TIME) AS LC_END_DATE
-    FROM lc_register lcj
+    FROM lc_add lca
     LEFT JOIN lc_removed lcr
-        ON lcj.LOYALTY_CARD_ID  = lcr.LOYALTY_CARD_ID
+        ON lca.LOYALTY_CARD_ID  = lcr.LOYALTY_CARD_ID
     WHERE
         EVENT_TYPE = 'SUCCESS'
 )
@@ -61,47 +61,51 @@ WITH mock_brands AS (
         ,lc.LC_START_DATE AS START_DATE
         ,LEAST(COALESCE(lc.LC_END_DATE, '9999-01-01'::DATE), COALESCE(u.U_DELETED_DATE, '9999-01-01'::DATE)) AS END_DATE
         ,b.BRAND
-        ,dlc.LOYALTY_PLAN_NAME
     FROM user_timespan u
     LEFT JOIN lc_start_end lc
         ON u.USER_ID = lc.USER_ID
         AND lc.LC_START_DATE >= u.U_CREATED_DATE
     LEFT JOIN mock_brands b
         ON u.USER_ID = b.USER_ID
-    LEFT JOIN dim_lc dlc
-        ON dlc.LOYALTY_CARD_ID = lc.LOYALTY_CARD_ID
     WHERE lc.USER_ID IS NOT NULL
 )
 
+,dedupe_users AS (
+    SELECT
+        USER_ID
+        ,MIN(START_DATE) AS START_DATE
+        ,MAX(END_DATE) AS END_DATE
+        ,BRAND
+    FROM users_start_end
+    GROUP BY
+        USER_ID
+        ,BRAND
+)
       
 ,u_creations as (
     SELECT
         START_DATE
         ,BRAND
-        ,LOYALTY_PLAN_NAME
         ,COUNT(*) C
     FROM
-        users_start_end
+        dedupe_users
     GROUP BY
         START_DATE
         ,BRAND
-        ,LOYALTY_PLAN_NAME
 )
 
 ,u_deletions as (
     SELECT
         END_DATE
         ,BRAND
-        ,LOYALTY_PLAN_NAME
         ,COUNT(*) C
     FROM
-        users_start_end
+        dedupe_users
     WHERE
         END_DATE != '9999-01-01'::DATE
     GROUP BY
         END_DATE
         ,BRAND
-        ,LOYALTY_PLAN_NAME
 )
   
 ,date_range AS (
@@ -110,21 +114,19 @@ WITH mock_brands AS (
     FROM
         dim_date
     WHERE
-        DATE >= (SELECT MIN(START_DATE) FROM users_start_end)
-        AND DATE <= (SELECT MAX(END_DATE) FROM users_start_end WHERE END_DATE != '9999-01-01'::DATE)
+        DATE >= (SELECT MIN(START_DATE) FROM dedupe_users)
+        AND DATE <= (SELECT MAX(END_DATE) FROM dedupe_users WHERE END_DATE != '9999-01-01'::DATE)
 )
   
 ,union_matching_records AS (
     SELECT
         START_DATE AS DATE
         ,BRAND
-        ,LOYALTY_PLAN_NAME
     FROM u_creations
     UNION
     SELECT
         END_DATE AS DATE
         ,BRAND
-        ,LOYALTY_PLAN_NAME
     FROM u_deletions
 ) 
 
@@ -132,22 +134,19 @@ WITH mock_brands AS (
     SELECT
         d.DATE
         ,r.BRAND
-        ,r.LOYALTY_PLAN_NAME
         ,COALESCE(uc.C,0) AS LC_USERS_CREATED
         ,COALESCE(ud.C,0) AS LC_USERS_DELETED
         ,LC_USERS_CREATED - LC_USERS_DELETED AS DAILY_CHANGE_IN_LC_USERS
-        ,SUM(DAILY_CHANGE_IN_LC_USERS) OVER (PARTITION BY r.BRAND, r.LOYALTY_PLAN_NAME ORDER BY d.DATE ASC) AS TOTAL_LC_USERS_COUNT
+        ,SUM(DAILY_CHANGE_IN_LC_USERS) OVER (PARTITION BY r.BRAND ORDER BY d.DATE ASC) AS TOTAL_LC_USERS_COUNT
     FROM date_range d
     LEFT JOIN union_matching_records r
         ON d.DATE = r.DATE
     LEFT JOIN u_creations uc
         ON d.DATE = uc.START_DATE
             AND r.BRAND = uc.BRAND
-            AND r.LOYALTY_PLAN_NAME = uc.LOYALTY_PLAN_NAME
     LEFT JOIN u_deletions ud
         ON d.DATE = ud.END_DATE
             AND r.BRAND = ud.BRAND
-            AND r.LOYALTY_PLAN_NAME = ud.LOYALTY_PLAN_NAME
 )
 
 SELECT *
@@ -155,4 +154,3 @@ FROM count_up
 ORDER BY
     DATE
     ,BRAND
-    ,LOYALTY_PLAN_NAME
