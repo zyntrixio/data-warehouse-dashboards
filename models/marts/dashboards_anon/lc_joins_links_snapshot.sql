02c0ca236e11c311640a705197d2a6d803f62f5e
Sam Pibworth <sam.pibworth@lumilinks.com>
2022-09-26 17:44:10 +0100

    new join links snapshot table

---
+++ models/marts/dashboards_anon/lc_joins_links_snapshot.sql
@@ -0,0 +1,123 @@
+WITH mock_brands AS (
+    SELECT *
+    FROM {{ref('trans__mock_brands')}}
+)
+
+,lc_joins AS (
+    SELECT *
+    FROM {{ref('trans__lc_joins_create_delete')}}
+)
+
+,lc_links AS (
+    SELECT *
+    FROM {{ref('trans__lc_links_create_delete')}}
+)
+
+,dim_lc AS (
+    SELECT *
+    FROM {{ref('src__dim_loyalty_card')}}
+)
+
+,dim_date AS (
+    SELECT *
+    FROM {{ref('src__dim_date')}}
+    WHERE
+        DATE >= (SELECT MIN(d) FROM (SELECT EVENT_DATE_TIME::DATE d FROM lc_joins UNION SELECT EVENT_DATE_TIME::DATE d FROM lc_links))
+        AND DATE <= CURRENT_DATE()
+)
+
+,union_joins_links AS (
+    SELECT
+        EVENT_ID
+        ,EVENT_DATE_TIME
+        ,EVENT_TYPE
+        ,LOYALTY_CARD_ID
+        ,CHANNEL
+        ,USER_ID
+        ,'JOIN' AS ADD_JOURNEY
+    FROM
+        lc_joins
+    UNION ALL
+    SELECT
+        EVENT_ID
+        ,EVENT_DATE_TIME
+        ,EVENT_TYPE
+        ,LOYALTY_CARD_ID
+        ,CHANNEL
+        ,USER_ID
+        ,'LINK' AS ADD_JOURNEY
+    FROM
+        lc_links
+)
+
+,add_loyalty_plan_and_brand AS (
+    SELECT
+        lc.*
+        ,b.BRAND
+        ,dlc.LOYALTY_PLAN_NAME
+    FROM union_joins_links lc
+    LEFT JOIN mock_brands b
+        ON lc.USER_ID = b.USER_ID
+    LEFT JOIN dim_lc dlc
+        ON lc.LOYALTY_CARD_ID = dlc.LOYALTY_CARD_ID
+)
+
+,event_ordering AS (
+    SELECT
+        *
+        ,COALESCE( LEAD(EVENT_DATE_TIME, 1) OVER (PARTITION BY ADD_JOURNEY, LOYALTY_PLAN_NAME, USER_ID ORDER BY EVENT_DATE_TIME), '9999-12-31'::DATE ) AS VALID_TO
+        ,LEAD(EVENT_TYPE, 1) OVER (PARTITION BY ADD_JOURNEY, LOYALTY_PLAN_NAME, USER_ID ORDER BY EVENT_DATE_TIME) AS NEXT_EVENT
+        ,LAG(EVENT_TYPE, 1 ) OVER (PARTITION BY ADD_JOURNEY, LOYALTY_PLAN_NAME, USER_ID ORDER BY EVENT_DATE_TIME) AS PREV_EVENT
+    FROM add_loyalty_plan_and_brand
+    QUALIFY
+        ((COALESCE(PREV_EVENT,'') != EVENT_TYPE )
+        OR (NEXT_EVENT IS NULL AND EVENT_TYPE IS NULL))
+)
+
+,day_ends AS ( -- just get events that finish the day
+    SELECT *
+    FROM event_ordering
+    WHERE
+        EVENT_DATE_TIME::date != VALID_TO::date
+)
+
+,lc_start_end as (
+    SELECT
+        EVENT_TYPE
+        ,ADD_JOURNEY
+        ,DATE(EVENT_DATE_TIME) AS START_DATE
+        ,DATEADD(day, -1, DATE(VALID_TO)) AS END_DATE
+        ,BRAND
+        ,LOYALTY_PLAN_NAME
+    FROM day_ends
+)
+
+,count_up AS (
+    SELECT
+        d.DATE
+        ,lc.BRAND
+        ,lc.LOYALTY_PLAN_NAME
+        ,COALESCE(SUM(CASE WHEN EVENT_TYPE = 'SUCCESS' AND ADD_JOURNEY = 'JOIN' THEN 1 END),0) AS JOIN_SUCCESS_STATE
+        ,COALESCE(SUM(CASE WHEN EVENT_TYPE = 'FAILED' AND ADD_JOURNEY = 'JOIN' THEN 1 END),0) AS JOIN_FAILED_STATE
+        ,COALESCE(SUM(CASE WHEN EVENT_TYPE = 'REQUEST' AND ADD_JOURNEY = 'JOIN' THEN 1 END),0) AS JOIN_REQUEST_STATE
+        ,COALESCE(SUM(CASE WHEN EVENT_TYPE = 'REMOVED' AND ADD_JOURNEY = 'JOIN' THEN 1 END),0) AS JOIN_REMOVED_STATE
+        ,COALESCE(SUM(CASE WHEN EVENT_TYPE = 'SUCCESS' AND ADD_JOURNEY = 'LINK' THEN 1 END),0) AS LINK_SUCCESS_STATE
+        ,COALESCE(SUM(CASE WHEN EVENT_TYPE = 'FAILED' AND ADD_JOURNEY = 'LINK' THEN 1 END),0) AS LINK_FAILED_STATE
+        ,COALESCE(SUM(CASE WHEN EVENT_TYPE = 'REQUEST' AND ADD_JOURNEY = 'LINK' THEN 1 END),0) AS LINK_REQUEST_STATE
+        ,COALESCE(SUM(CASE WHEN EVENT_TYPE = 'REMOVED' AND ADD_JOURNEY = 'LINK' THEN 1 END),0) AS LINK_REMOVED_STATE
+    FROM lc_start_end lc
+    LEFT JOIN DIM_DATE d
+        ON d.DATE >= lc.START_DATE
+        AND d.DATE < lc.END_DATE
+    GROUP BY
+        d.DATE
+        ,lc.BRAND
+        ,lc.LOYALTY_PLAN_NAME
+    HAVING
+        DATE IS NOT NULL
+        AND LOYALTY_PLAN_NAME IS NOT NULL
+        AND BRAND IS NOT NULL
+)
+
+SELECT *
+FROM count_up
