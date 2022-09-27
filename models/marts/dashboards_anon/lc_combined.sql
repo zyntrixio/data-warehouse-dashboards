4ea9164e1721cb0d1dff674781a0e5f6bb0ede85
Sam Pibworth <sam.pibworth@lumilinks.com>
2022-09-27 17:23:35 +0100

    add a combined table

---
+++ models/marts/dashboards_anon/lc_combined.sql
@@ -0,0 +1,57 @@
+WITH lc_joins_links AS (
+    SELECT *
+    FROM {{ref('lc_joins_links')}}
+)
+
+,lc_joins_links_snapshot AS (
+    SELECT *
+    FROM {{ref('lc_joins_links_snapshot')}}
+)
+
+,base_table AS (
+    SELECT DISTINCT
+        DATE
+        ,BRAND
+        ,LOYALTY_PLAN_NAME
+    FROM lc_joins_links
+    UNION
+    SELECT DISTINCT
+        DATE
+        ,BRAND
+        ,LOYALTY_PLAN_NAME
+    FROM lc_joins_links_snapshot
+)
+
+,combine AS (
+    SELECT
+        b.DATE
+        ,b.BRAND
+        ,b.LOYALTY_PLAN_NAME
+        ,COALESCE(jl.JOIN_REQUEST_PENDING, 0) AS JOIN_REQUEST_PENDING_EVENT
+        ,COALESCE(jl.JOIN_FAILED, 0) AS JOIN_FAILED_EVENT
+        ,COALESCE(jl.JOIN_SUCCESSFUL, 0) AS JOIN_SUCCESSFUL_EVENT
+        ,COALESCE(jl.LINK_REQUEST_PENDING, 0) AS LINK_REQUEST_PENDING_EVENT
+        ,COALESCE(jl.LINK_FAILED, 0) AS LINK_FAILED_EVENT
+        ,COALESCE(jl.LINK_SUCCESSFUL, 0) AS LINK_SUCCESSFUL_EVENT
+        ,COALESCE(jls.JOIN_SUCCESS_STATE, 0) AS JOIN_SUCCESS_STATE
+        ,COALESCE(jls.JOIN_FAILED_STATE, 0) AS JOIN_FAILED_STATE
+        ,COALESCE(jls.JOIN_PENDING_STATE, 0) AS JOIN_PENDING_STATE
+        ,COALESCE(jls.JOIN_REMOVED_STATE, 0) AS JOIN_REMOVED_STATE
+        ,COALESCE(jls.LINK_SUCCESS_STATE, 0) AS LINK_SUCCESS_STATE
+        ,COALESCE(jls.LINK_FAILED_STATE, 0) AS LINK_FAILED_STATE
+        ,COALESCE(jls.LINK_PENDING_STATE, 0) AS LINK_PENDING_STATE
+        ,COALESCE(jls.LINK_REMOVED_STATE, 0) AS LINK_REMOVED_STATE
+    FROM
+        base_table b
+    LEFT JOIN lc_joins_links jl ON
+        b.DATE = jl.DATE
+        AND b.BRAND = jl.BRAND
+        AND b.LOYALTY_PLAN_NAME = jl.LOYALTY_PLAN_NAME
+    LEFT JOIN lc_joins_links_snapshot jls ON
+        b.DATE = jls.DATE
+        AND b.BRAND = jls.BRAND
+        AND b.LOYALTY_PLAN_NAME = jls.LOYALTY_PLAN_NAME
+)
+
+SELECT *
+FROM combine
