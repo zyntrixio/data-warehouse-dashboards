569ef61893a2ef518f78247bfcaff932565dad5d
Sam Pibworth <sam.pibworth@lumilinks.com>
2022-09-30 16:32:14 +0100

    restructure errors

--- models/marts/dashboards_anon/errors.sql
+++ models/marts/dashboards_anon/errors.sql
@@ -26,6 +26,18 @@ WITH mock_brands AS (
         AND DATE <= CURRENT_DATE()
 )
 
+,add_partitioning_columns AS ( -- Join in brands and loyalty plan.
+    SELECT
+        lc.*
+        ,b.BRAND
+        ,dlc.LOYALTY_PLAN_COMPANY
+    FROM lc_sc lc
+    LEFT JOIN mock_brands b
+        ON lc.USER_ID = b.USER_ID
+    LEFT JOIN dim_lc dlc
+        ON lc.LOYALTY_CARD_ID = dlc.LOYALTY_CARD_ID 
+)
+
 ,event_ordering AS ( -- Get Future And previous events per LC & User
     SELECT
         EVENT_DATE_TIME AS STATUS_START_TIME
@@ -34,10 +46,14 @@ WITH mock_brands AS (
         ,CHANNEL
         ,LOYALTY_CARD_ID
         ,USER_ID
-        ,LEAD(EVENT_DATE_TIME, 1) OVER (PARTITION BY LOYALTY_CARD_ID, USER_ID ORDER BY EVENT_DATE_TIME) AS STATUS_END_TIME
-        ,LEAD(TO_STATUS_ID, 1) OVER (PARTITION BY LOYALTY_CARD_ID, USER_ID ORDER BY EVENT_DATE_TIME) AS NEXT_STATUS_ID
-        ,LAG(TO_STATUS_ID, 1 ) OVER (PARTITION BY LOYALTY_CARD_ID, USER_ID ORDER BY EVENT_DATE_TIME) AS PREV_STATUS_ID
-    FROM lc_sc
+        ,BRAND
+        ,LOYALTY_PLAN_NAME
+        ,LOYALTY_PLAN_COMPANY
+        ,LEAD(EVENT_DATE_TIME, 1) OVER (PARTITION BY LOYALTY_PLAN_NAME, USER_ID ORDER BY EVENT_DATE_TIME) AS STATUS_END_TIME
+        ,LEAD(TO_STATUS_ID, 1) OVER (PARTITION BY LOYALTY_PLAN_NAME, USER_ID ORDER BY EVENT_DATE_TIME) AS NEXT_STATUS_ID
+        ,LAG(TO_STATUS_ID, 1 ) OVER (PARTITION BY LOYALTY_PLAN_NAME, USER_ID ORDER BY EVENT_DATE_TIME) AS PREV_STATUS_ID
+    FROM
+        add_partitioning_columns
 )
 
 ,join_status_types AS ( -- Join in lookup table to determine which status' are errors
@@ -76,7 +92,7 @@ WITH mock_brands AS (
         ,CASE WHEN
             COALESCE(
                 SUM(CASE WHEN STATUS_TYPE = 'Success' THEN 1 ELSE 0 END)
-                OVER (PARTITION BY LOYALTY_CARD_ID, USER_ID ORDER BY STATUS_START_TIME
+                OVER (PARTITION BY LOYALTY_PLAN_NAME, USER_ID ORDER BY STATUS_START_TIME
                 ROWS BETWEEN 1 FOLLOWING AND UNBOUNDED FOLLOWING ), 0
                 ) >= 1 THEN TRUE
             ELSE FALSE
@@ -93,34 +109,30 @@ WITH mock_brands AS (
     FROM join_status_types
 )
 
-,add_partitioning_columns AS ( -- Join in brands and loyalty plan. Filter out all non Error events
+,filter_non_error_events AS ( -- Filter out all non Error events
     SELECT
-        lc.STATUS_ID
-        ,lc.STATUS_DESCRIPTION
-        ,lc.STATUS_GROUP
-        ,lc.STATUS_ROLLUP
-        ,lc.USER_ID
-        ,b.BRAND
-        ,dlc.LOYALTY_PLAN_NAME
-        ,dlc.LOYALTY_PLAN_COMPANY
-        ,lc.REPEATED_STATUS
-        ,lc.TO_RESOLVED
-        ,lc.IS_RESOLVED
-        ,lc.IS_FINAL_STATE
-        ,lc.STATUS_START_TIME
-        ,lc.STATUS_END_TIME
-        ,lc.TIMEDIFF_DAYS
-        ,lc.TIMEDIFF_HOURS
-        ,lc.TIMEDIFF_MINS
-        ,lc.TIMEDIFF_SECONDS
-        ,lc.TIMEDIFF_MILLISECONDS  
+        STATUS_ID
+        ,STATUS_DESCRIPTION
+        ,STATUS_GROUP
+        ,STATUS_ROLLUP
+        ,USER_ID
+        ,BRAND
+        ,LOYALTY_PLAN_NAME
+        ,LOYALTY_PLAN_COMPANY
+        ,REPEATED_STATUS
+        ,TO_RESOLVED
+        ,IS_RESOLVED
+        ,IS_FINAL_STATE
+        ,STATUS_START_TIME
+        ,STATUS_END_TIME
+        ,TIMEDIFF_DAYS
+        ,TIMEDIFF_HOURS
+        ,TIMEDIFF_MINS
+        ,TIMEDIFF_SECONDS
+        ,TIMEDIFF_MILLISECONDS  
     FROM add_metrics lc
-    LEFT JOIN mock_brands b
-        ON lc.USER_ID = b.USER_ID
-    LEFT JOIN dim_lc dlc
-        ON lc.LOYALTY_CARD_ID = dlc.LOYALTY_CARD_ID 
-    WHERE lc.STATUS_TYPE = 'Error'
+    WHERE STATUS_TYPE = 'Error'
 )
 
 SELECT *
-FROM add_partitioning_columns
+FROM filter_non_error_events
