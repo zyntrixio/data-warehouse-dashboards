e78a17ee3790f7e24f83701da935a2a325250611
Sam Pibworth <sam.pibworth@lumilinks.com>
2022-10-04 16:04:29 +0100

    basic aggregation

---
+++ models/marts/dashboards_anon/user_loyalty_plan_status_snapshot.sql
@@ -0,0 +1,39 @@
+WITH user_statuses AS (
+    SELECT *
+    FROM {{ref('user_loyalty_plan_status')}}
+)
+
+,dim_date AS (
+    SELECT *
+    FROM {{ref('src__dim_date')}}
+    WHERE
+        DATE >= (SELECT MIN(STATUS_FROM_DATE) FROM user_statuses)
+        AND DATE <= CURRENT_DATE()
+)
+
+,count_up AS (
+    SELECT
+        d.DATE
+        ,u.BRAND
+        ,u.LOYALTY_PLAN
+        ,COALESCE(SUM(CASE WHEN u.CURRENT_STATUS = 'REGISTRATION' THEN 1 END),0) AS REGISTRATION_STATE
+        ,COALESCE(SUM(CASE WHEN u.CURRENT_STATUS = 'ACTIVE' THEN 1 END),0) AS ACTIVE_STATE
+        ,COALESCE(SUM(CASE WHEN u.CURRENT_STATUS = 'INACTIVE' THEN 1 END),0) AS INACTIVE_STATE
+        ,COALESCE(SUM(CASE WHEN u.CURRENT_STATUS = 'DORMANT' THEN 1 END),0) AS DORMANT_STATE        
+    FROM user_statuses u
+    LEFT JOIN dim_date d
+        ON d.DATE >= u.STATUS_FROM_DATE
+        AND d.DATE < u.STATUS_TO_DATE
+    GROUP BY
+        d.DATE
+        ,u.BRAND
+        ,u.LOYALTY_PLAN
+    HAVING
+        REGISTRATION_STATE != 0
+        OR ACTIVE_STATE != 0
+        OR INACTIVE_STATE != 0
+        OR DORMANT_STATE != 0
+)
+
+SELECT *
+FROM count_up
