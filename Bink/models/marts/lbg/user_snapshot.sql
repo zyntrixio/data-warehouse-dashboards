403722fd11310462d0added669b3c63c965909a9
Anand Bhakta <abhakta@bink.com>
2023-02-01 17:03:02 +0000

    Create user_snapshot data source only including reg/dereg

---
+++ Bink/models/marts/lbg/user_snapshot.sql
@@ -0,0 +1,87 @@
+WITH user_statuses AS (
+    SELECT *
+    FROM {{ref('trans__lbg_user')}}
+    WHERE EVENT_TYPE IN ('CREATED', 'DELETED')
+)
+
+,dim_date AS (
+    SELECT *
+    FROM {{ref('src__dim_date')}}
+    WHERE
+        DATE >= (SELECT MIN(STATUS_FROM_DATE) FROM user_statuses)
+        AND DATE <= CURRENT_DATE()
+)
+
+,to_from_dates AS (
+    SELECT
+        USER_ID
+        ,BRAND
+        ,EXTERNAL_USER_REF
+        ,EVENT AS FROM_EVENT
+        ,DATE(EVENT_DATE_TIME) AS FROM_DATE
+        ,DATE(LEAD(EVENT_DATE_TIME) OVER (PARTITION BY EXTERNAL_USER_REF ORDER BY EVENT_DATE_TIME)) AS TO_DATE
+        ,LEAD(EVENT) OVER (PARTITION BY EXTERNAL_USER_REF ORDER BY EVENT_DATE_TIME) AS TO_EVENT
+    FROM 
+    user_statuses
+)
+
+
+,count_up AS (
+  SELECT
+    d.DATE
+    ,u.BRAND
+    ,COALESCE(SUM(CASE WHEN u.FROM_EVENT = 'CREATED' THEN 1 END),0) AS REGISTERED_USERS
+    ,COALESCE(SUM(CASE WHEN u.FROM_EVENT = 'DELETED' THEN 1 END),0) AS DEREGISTERED_USERS
+FROM to_from_dates u
+LEFT JOIN dim_date d
+    ON d.DATE >= u.FROM_DATE
+    AND d.DATE < COALESCE(u.TO_DATE, '9999-12-31')
+GROUP BY
+    d.DATE
+    ,u.BRAND
+ORDER BY
+    d.DATE
+    ,u.BRAND
+)
+
+SELECT *
+FROM count_up;
+
+
+
+
+
+
+
+
+
+
+
+-- ,count_up AS (
+--     SELECT
+--         d.DATE
+--         ,u.BRAND
+--         ,COALESCE(SUM(CASE WHEN u.CURRENT_STATUS = 'ACTIVE' THEN 1 END),0) AS ACTIVE_STATE
+--         ,COALESCE(SUM(CASE WHEN u.CURRENT_STATUS = 'INACTIVE' OR u.CURRENT_STATUS = 'REGISTRATION' THEN 1 END),0) AS INACTIVE_STATE
+--         ,COALESCE(SUM(CASE WHEN u.CURRENT_STATUS = 'DORMANT' AND u.DAYS_SINCE_REGISTRATION >= 30 THEN 1 END),0) AS DORMANT_STATE
+--         ,COALESCE(SUM(CASE WHEN u.CURRENT_STATUS IN ('REGISTRATION', 'ACTIVE', 'INACTIVE', 'DORMANT') THEN 1 END),0) AS REGISTERED_USERS
+--         ,COALESCE(SUM(CASE WHEN u.CURRENT_STATUS = 'REMOVED' THEN 1 END),0) AS DEREGISTERED_USERS
+--     FROM user_statuses u
+--     LEFT JOIN dim_date d
+--         ON d.DATE >= u.STATUS_FROM_DATE
+--         AND d.DATE < COALESCE(u.STATUS_TO_DATE,'9999-12-31')
+--     GROUP BY
+--         d.DATE
+--         ,u.BRAND
+--     HAVING
+--         DATE IS NOT NULL
+--         AND
+--         (ACTIVE_STATE != 0
+--         OR INACTIVE_STATE != 0
+--         OR DORMANT_STATE != 0
+--         OR REGISTERED_USERS != 0
+--         OR DEREGISTERED_USERS != 0)
+-- )
+
+-- SELECT *
+-- FROM count_up
